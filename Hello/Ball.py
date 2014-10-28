96495c830d82d730ba767353461eb387b705f2ea
Luyang Li <liluyang@gmail.com>
2014-10-27 17:30:00 -0700

    .

--- Hello/Ball.py
+++ Hello/Ball.py
@@ -1,6 +1,6 @@
 from Tkinter import *
+from random import randint
 
-import random
 import time
 
 class Ball:
@@ -8,24 +8,22 @@ class Ball:
     self.canvas = canvas
     self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
     self.canvas.move(self.id, 245, 100)
-    starts = [-3, -2, -1, 1, 2, 3]
-    random.shuffle(starts)
-    self.x = starts[0]
-    self.y = -3
+    self.x = randint(-3, 3)
+    self.y = randint(-3, 3)
     self.canvas.height = self.canvas.winfo_height()
     self.canvas.width = self.canvas.winfo_width()
 
   def draw(self):
-    self.canvas.move(self.id, 0, -1)
+    self.canvas.move(self.id, self.x, self.y)
     pos = self.canvas.coords(self.id)
-    if pos[1] <= 0:
-      self.y = 1
-    if pos[3] >= self.canvas.height:
-      self.y = -1
     if pos[0] <= 0:
-      self.y = -3
-    if pos[2] >= self.canvas_width:
-      self.x = -3
+      self.x = randint(1, 3)
+    if pos[1] <= 0:
+      self.y = randint(1, 3)
+    if pos[2] >= self.canvas.width:
+      self.x = randint(-3, -1)
+    if pos[3] >= self.canvas.height:
+      self.y = randint(-3, -1)
 
 tk = Tk()
 tk.resizable(0, 0)
