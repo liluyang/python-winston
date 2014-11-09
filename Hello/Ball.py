c2687743682a93cc98de89074b629156b74d838d
winston <thestarwinston@gmail.com>
2014-11-08 19:10:10 -0500

    .

--- Hello/Ball.py
+++ Hello/Ball.py
@@ -31,7 +31,7 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=400, height=400)
 canvas.pack()
 tk.update()
-ball = Ball(canvas, 'red')
+ball = Ball(canvas, 'blue')
 
 while 1:
   ball.draw()
